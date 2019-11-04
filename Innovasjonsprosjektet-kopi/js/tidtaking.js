b254eafe5e0a533e21a2294572b49589bc76ac39
vegardinho <landsverk.vegard@gmail.com>
2019-11-04 09:19:42 +0100

    Lagt til ny tidtakingssystem og endre navn på banene

--- Innovasjonsprosjektet-kopi/js/tidtaking.js
+++ Innovasjonsprosjektet-kopi/js/tidtaking.js
@@ -1,8 +1,40 @@
-setInterval(counter, 1000);
+var hvitTid = 2160;
+var roead_tid = 2160;
+var hvitRunde = true;
+var stoppet = true;
 
 function counter() {
-
+   if (hvitRunde) {
+      endreTid(hvitTid);
+      document.getElementById("hvitTid").innerHTML = tid_streng;
+   } else {
+      endreTid(roedTid);
+      document.getElementById("roedTid").innerHTML = tid_streng;
+   }
 }
 
-document.getElementById("start_stop").addEventListener("click", 
+function endreTid(tid) {
+   tid --;
+   tidStreng = lagTidStreng(tid);
+}
+
+function lagTidStreng(tid) {
+   var min = Math.floor(tid/60);
+   var sek = tid % 60;
+   var streng = `${min}:${sek}`;
+   return streng;
+}
+
+// Start og stopp timer ved klikk
+document.getElementById("start_stop").addEventListener("click", function(event) {
+   if (stoppet) {
+      intervall = setInterval(counter, 1000);
+      stoppet = false;
+   } else {
+      clearInterval(intervall);
+      stoppet = true;
+   }
+});
+
+document.getElementById("bytt_lag").addEventListener("click:wqa
 

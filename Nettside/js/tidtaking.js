9c3454533e429026b3ed4481f262106891d4379f
vegardinho <landsverk.vegard@gmail.com>
2019-11-18 23:38:33 +0100

    Fjernet feil ting i tidtaking

--- Nettside/js/tidtaking.js
+++ Nettside/js/tidtaking.js
@@ -30,42 +30,6 @@ function roed_time() {
       this.style.backgroundColor = "#f44336";
    }
 }
-// sjekk om nytt i json-fil, og endre etter dette
-// intervall = setInterval(lesJson, 1);
-
-// var requestURL = 'data.json';
-// var request = new XMLHttpRequest();
-// request.open('GET', requestURL);
-// request.responseType = 'json';
-// request.send();
-// request.onload = function() {
-//    var svar = request.response;
-//
-// };
-
-function loadJSON(callback) {   
-
-   var xobj = new XMLHttpRequest();
-   xobj.overrideMimeType("application/json");
-   xobj.open('GET', 'data.json', true); // Replace 'my_data' with the path to your file
-   xobj.onreadystatechange = function () {
-      if (xobj.readyState == 4 && xobj.status == "200") {
-	 // Required use of an anonymous callback as .open will NOT
-	 // return a value but simply returns undefined in asynchronous mode
-	 callback(xobj.responseText);
-      }
-   };
-   xobj.send(null);  
-}
-
-   loadJSON(function(response) {
-      // Parse JSON string into object
-      var actual_JSON = JSON.parse(response);
-   });
-
-// function lesJson() {
-//    console.log(jsonData[0].aks);
-// }
 
 // Trekk fra et sekund hvert sekund og oppdater siden
 function counter() {
